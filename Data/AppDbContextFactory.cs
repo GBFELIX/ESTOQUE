5ce8e4f7846ec2e35d5c2b7f6c92a2c4a7780b7b
Gabriel Félix <felixgabriel.sant@gmail.com>
2025-05-30 17:59:42 -0300

    UP

--- Data/AppDbContextFactory.cs
+++ Data/AppDbContextFactory.cs
@@ -8,17 +8,14 @@ namespace Treinando.Data
     public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
     {
         public AppDbContext CreateDbContext(string[] args)
-        {
-            // Lê o appsettings.json
+        { 
             IConfigurationRoot configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory()) // base na raiz do projeto
+                .SetBasePath(Directory.GetCurrentDirectory()) 
                 .AddJsonFile("appsettings.json")
                 .Build();
-
-            // Pega a string de conexão
+                
             var connectionString = configuration.GetConnectionString("DefaultConnection");
-
-            // Configura o DbContext com a string
+           
             var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
             optionsBuilder.UseSqlServer(connectionString);
 
